3835c9a7d648e0bd3649b320b7be0f79906a8237
Junyan Li <lijunyan668@outlook.com>
2022-06-27 10:42:53 +0800

    update

--- LeViT/build_engine_run_plugin.sh
+++ LeViT/build_engine_run_plugin.sh
@@ -4,8 +4,8 @@ mkdir -p trt_plans
 cd ./plugin
 make clean && make 
 cd ..
-python trt_main_build.py --model="128S"
+python3 trt_main_build.py --model="128S"
 
-python pyTorchToTensorRT0.py
-python pyTorchToTensorRT.py
-python trt_main_softmax.py --model="128S"
+python3 pyTorchToTensorRT0.py
+python3 pyTorchToTensorRT.py
+python3 trt_main_softmax.py --model="128S"
